bde88ab3e0b763a06b3206c6d5894f26e8aad5fd
Michal Moczulski <moczul@gmail.com>
2018-03-18 21:31:38 +0100

    Set script messages

--- verify-build.sh
+++ verify-build.sh
@@ -2,6 +2,8 @@
 
 if [ -n "$DOCKER_GRADLE_CACHE" ]
 then
+        echo "Running docker with gradle cache"
+
         docker run --rm  \
                 -v $DOCKER_GRADLE_CACHE:/docker-gradle-cache \
                 -e "GRADLE_USER_HOME=/docker-gradle-cache" \
@@ -9,6 +11,8 @@ then
                 -w /workspace \
                 michalmoczulski/simple-android-docker gradle clean build
 else
+        echo "Consider setting DOCKER_GRADLE_CACHE variable for gradle caching"
+
         docker run --rm  \
                 -v $(pwd):/workspace \
                 -w /workspace \
